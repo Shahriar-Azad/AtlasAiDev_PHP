fdbb376b53849f10cebaacec88cdb83d69c3f321
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-10-21 18:21:26 +0600

    numbers, type casting

--- index.php
+++ index.php
@@ -35,3 +35,29 @@ echo "Position of 'World': " . strpos($fullStr, "World") . "<br>";
 echo "Replace: " . str_replace("World", "PHP", $fullStr) . "<br><br>";
 
 
+// ----- 3. NUMBERS -----
+echo "<h2>3. Numbers</h2>";
+$a = 10;
+$b = 3;
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b) . "<br>";
+echo "Modulus: " . ($a % $b) . "<br>";
+echo "Power: " . pow($a, $b) . "<br><br>";
+
+
+// ----- 4. TYPE CASTING -----
+echo "<h2>4. Type Casting</h2>";
+$intVar = 42;
+$floatVar = (float)$intVar;
+$stringVar = (string)$intVar;
+$boolVar = (bool)$intVar;
+echo "Integer: $intVar<br>";
+echo "To Float: $floatVar<br>";
+echo "To String: $stringVar<br>";
+echo "To Boolean: ";
+var_dump($boolVar);
+echo "<br><br>";
+
+
